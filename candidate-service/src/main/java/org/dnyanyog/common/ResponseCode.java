f0238d735b7b5c0c7915b461ba76c5043bcd75d2
Himanshh20 <137694881+Himanshh20@users.noreply.github.com>
2025-02-05 23:42:34 +0530

    Refactor :Introduce CandidateMapper for clean data transformation
    * Improved code readability in the candidate service
    * Refactored methods to reduce duplication and improve maintainability

--- candidate-service/src/main/java/org/dnyanyog/common/ResponseCode.java
+++ candidate-service/src/main/java/org/dnyanyog/common/ResponseCode.java
@@ -1,11 +1,11 @@
 package org.dnyanyog.common;
 
 public enum ResponseCode {
-  Add_Candidate("0000", "Candidate Added Sucessfully"),
-  Update_Candidate("0000", " Candidate Updated Sucessfully"),
-  Search_Candidate("0000", " Candidate Found "),
-  Delete_Candidate("0000", "Candidate Deleted Sucessfully"),
-  Candidate_Not_Found("911", "Candidate Not Found");
+  Add_Candidate("200", "Candidate Added Sucessfully"),
+  Update_Candidate("200", " Candidate Updated Sucessfully"),
+  Search_Candidate("200", " Candidate Found "),
+  Delete_Candidate("200", "Candidate Deleted Sucessfully"),
+  Candidate_Not_Found("404", "Candidate Not Found");
 
   private final String code;
   private final String message;
